c94fc3a6b3272463605dd45dd0a00adce04f9ad0
cpuddle <collin.franklin.2003@gmail.com>
2025-04-23 13:33:32 -0500

    Starting the backend work

---
+++ backend/api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.responses import RedirectResponse
+import pymysql.cursors
+import uvicorn
+from initScripts.mysqlDb import MysqlDb
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.responses import JSONResponse
+from fastapi.responses import StreamingResponse
+
+
+install()
+console = Console()
+
+import os
+os.chdir("...")
+
+# Customize traceback
+install(show_locals=True)  # Show local variables in tracebacks
+
+
